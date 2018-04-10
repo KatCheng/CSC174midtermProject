25225c0f6e7e803e6ca2c8543bb6f4d5cbf96ab6
kat <syzcitrine@gmail.com>
2018-04-09 21:04:12 -0400

    now passes validators

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
       <meta charset="utf-8">
       <title>Crayon Shin-chan</title>
-      <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1">
       <link href="https://fonts.googleapis.com/css?family=Dokdo|Gloria+Hallelujah|Indie+Flower|Jua|Kalam" rel="stylesheet">
       <link rel="stylesheet" type="text/css" href="./css/normalize.min.css">
       <link rel="stylesheet" type="text/css" href="./css/styles.css">
